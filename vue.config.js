2f714abeadf32dadbacec8c48cb3b87001efb3b4
lijiwei <lijiwei@fenbi.com>
2021-01-11 11:10:14 +0800

    test manifest

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,21 @@
 const path = require('path')
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 module.exports = {
   lintOnSave: false,
+  publicPath: '/test/',
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'less',
       patterns: [
         path.resolve(__dirname, './src/common/style/less-variables.less'),
+      ]
+    }
+  },
+  configureWebpack: config => {
+    return {
+      plugins: [
+        new WebpackManifestPlugin()
       ]
     }
   }
